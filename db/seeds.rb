8a5f9cf72bb465c84ee9a31125062c60fef5e9e6
Martin LinuxVBox <marting12@live.dk>
2016-05-26 14:01:57 +0200

    Adds admin user in seed and updates todo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+User.create(email: "marting12@live.dk", password: "hejhejhej", admin: true)
 
